c0c1c64dc87d23dc89b635b245e355a1357899a7
Chetan Saini <saini.chetan09@gmail.com>
2025-06-02 15:17:30 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  It is build, deployed using jenkins
+          Edit <code>src/App.js</code> and save to reload. this is build, deployed using jenkins
         </p>
         <a
           className="App-link"
